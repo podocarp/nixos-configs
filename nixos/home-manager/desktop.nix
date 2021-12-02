b9f1bfc7e28bea27d0e86c3f6aa0c0c4fa337bf4
J XD <xdjiaxd@gmail.com>
2021-12-02 21:24:37 +0800

    edits

--- nixos/home-manager/desktop.nix
+++ nixos/home-manager/desktop.nix
@@ -138,14 +138,12 @@ in
   };
 
   # This must be enabled for fonts to be installed through packages.
-  fonts.fontconfig.enable = true;
+  # fonts.fontconfig.enable = true;
+
+  i18n.inputMethod.enabled = "fcitx";
+  i18n.inputMethod.fcitx.engines = with pkgs.fcitx-engines; [ libpinyin ];
 
   nixpkgs.config.packageOverrides = pkgs : {
-    # nur = import
-    #   (builtins.fetchTarball
-    #     "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-    #   inherit pkgs;
-    # };
   };
 
   # xdg.configFile."plasma-workspace/env/set_window_manager.sh" = {
@@ -155,31 +153,18 @@ in
     enable = true;
     windowManager.command = lib.mkForce "exec startplasma-x11";
     initExtra = ''
-      # export XMODIFIERS = "@im=fcitx"
-      # export XMODIFIER = "@im=fcitx"
-      # export GTK_IM_MODULE = "@im=fcitx"
-      # export QT_IM_MODULE = "@im=fcitx"
+      export XMODIFIERS = "@im=fcitx"
+      export XMODIFIER = "@im=fcitx"
+      export GTK_IM_MODULE = "@im=fcitx"
+      export QT_IM_MODULE = "@im=fcitx"
     '';
     profileExtra = ''
      autorandr -c
     '';
-    # pointerCursor = {
-    #   package = pkgs.vanilla-dmz;
-    #   name = "Vanilla-DMZ";
-    #   size = 64;
-    # };
     scriptPath = ".xsession-hm";
   };
 
   xresources.extraConfig =
-  # builtins.readFile (
-  #   pkgs.fetchFromGitHub {
-  #     owner = "morhetz";
-  #     repo = "gruvbox-contrib";
-  #     rev = "master";
-  #     sha256 = "181irx5jas3iqqdlc6v34673p2s6bsr8l0nqbs8gsv88r8q066l6";
-  #   } + "/xresources/gruvbox-light.xresources"
-  #   ) +
   ''
       ! PaperColor Theme
       *.foreground: #4D4D4C

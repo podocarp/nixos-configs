7662782ba58294dc5d7f8c98be076ed6b89a41ff
J XD <xdjiaxd@gmail.com>
2021-12-02 21:24:37 +0800

    edits

--- nixos/machines/desktop.nix
+++ nixos/machines/desktop.nix
@@ -14,31 +14,6 @@
 
   networking.hostName = "desktop"; # Define your hostname.
   networking.firewall.allowedUDPPorts = [ 50000 ];
-  networking.wireguard = {
-    enable = false;
-    interfaces = {
-      wg0 = {
-        ips = [ "10.0.0.1/24" ];
-        listenPort = 50000;
-
-        privateKey =
-          builtins.elemAt 0
-            (lib.strings.splitString "\n"
-              (builtins.extraBuiltins.getSecret "nix/wireguard_client"));
-
-        peers = [
-          {
-            publicKey = "EnNBgGNhYPEWP+eb/uy4Ye4/YCxFCgy1kMQtb+H/yw4=";
-            allowedIPs = [ "0.0.0.0/0" ];
-            persistentKeepalive = 25;
-          }
-        ];
-      };
-    };
-  };
-
-  # nixpkgs.config.packageOverrides = pkgs: {
-  # };
 
   services.xserver.videoDrivers = [ "nvidia" ];
 
@@ -52,9 +27,14 @@
     ];
   };
 
+  programs.ssh.extraConfig = ''
+    HostKeyAlgorithms ssh-rsa
+    PubkeyAcceptedKeyTypes ssh-rsa
+  '';
+
   virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = [ "pengu" ];
-  home-manager.users.pengu = import ./home-manager/desktop.nix;
+  home-manager.users.pengu = import ../home-manager/desktop.nix;
 
   sound.enable = true;
   hardware.pulseaudio.enable = true;

8633cecf08e232ff4dfdcf3cd61deb3f6da34dbf
J XD <xdjiaxd@gmail.com>
2022-01-06 12:08:08 +0800

    switch to ungoogled chromium

--- nixos/home-manager/programs/chromium/default.nix
+++ nixos/home-manager/programs/chromium/default.nix
@@ -3,11 +3,46 @@
 {
   programs.chromium = {
     enable = true;
-    package = pkgs.chromium;
-    extensions = [
-      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
-      { id = "pkehgijcmpdhfbdbbnkijodmdjhbjlgp"; } # privacy badger
-      { id = "djflhoibgkdhkhhcedjiklpkjnoahfmg"; } # user agent switcher
+    package = pkgs.ungoogled-chromium;
+    extensions =
+      let
+        getExt =
+          { id, sha256, version }:
+          {
+            inherit id;
+            inherit version;
+            crxPath = builtins.fetchurl {
+              inherit sha256;
+              name = "${id}.crx";
+              url = "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=${lib.versions.major pkgs.ungoogled-chromium.version}&x=id%3D${id}%26installsource%3Dondemand%26uc";
+            };
+          };
+      in
+    [
+      # clearurls
+      (getExt {
+        id = "lckanjgmijmafbedllaakclkaicjfmnk";
+        sha256 = "12vijbyhr137d1r57x0gj8fwfmyz400x8x853xdhzd4sn99w7fza";
+        version = "1.22.0";
+      })
+      # privacy badger
+      (getExt {
+        id = "pkehgijcmpdhfbdbbnkijodmdjhbjlgp";
+        sha256 = "0wgr2q7x01naqj1fh6wmam36x89dig7qdrr4xy043k6c0id7j776";
+        version = "2021.11.23.1";
+      })
+      # ublock origin
+      (getExt {
+        id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";
+        sha256 = "0jxkiq53l5q9c12gi630zmy367hh81xibr2wx1pw54i8n9hn8yqr";
+        version = "1.40.4";
+      })
+      # vimium
+      (getExt {
+        id = "dbepggeogbaibhgnhhndojpepiihcmeb";
+        sha256 = "097axwrhn8g26kp25w86x71khaqcw3nb0ras9ndwqvdw3bpgkcd8";
+        version = "1.67";
+      })
       { crxPath = pkgs.fetchFromGitHub {
           owner = "NeverDecaf";
           repo = "chromium-web-store";

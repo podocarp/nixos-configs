cbcb48ccf1a3cb3e06ba85bebaaaba7e20a9f376
J XD <xdjiaxd@gmail.com>
2022-02-07 23:27:52 +0800

    update fcitx

--- nixos/home-manager/misc/fcitx/default.nix
+++ nixos/home-manager/misc/fcitx/default.nix
@@ -1,11 +1,5 @@
 { pkgs, ... }:
 {
-  i18n.inputMethod.enabled = "fcitx5";
+  i18n.inputMethod.enabled = "fcitx";
   i18n.inputMethod.fcitx.engines = [ pkgs.fcitx-engines.libpinyin ];
-  xsession.initExtra = ''
-      export XMODIFIERS = "@im=fcitx"
-      export XMODIFIER = "@im=fcitx"
-      export GTK_IM_MODULE = "@im=fcitx"
-      export QT_IM_MODULE = "@im=fcitx"
-  '';
 }

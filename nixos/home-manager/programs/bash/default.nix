d87aa3f6494516c26131e4935d59079244bfed7b
J XD <xdjiaxd@gmail.com>
2022-03-21 09:54:46 +0800

    minor edits

--- nixos/home-manager/programs/bash/default.nix
+++ nixos/home-manager/programs/bash/default.nix
@@ -20,10 +20,6 @@
         set -o vi
         HISTCONTROL=ignoreboth
 
-        if [ -x "$(command -v tmux)" ] && [ -n "''${DISPLAY}" ]; then
-          [ -z "''${TMUX}" ] && { tmux; } >/dev/null 2>&1
-        fi
-
         PS1="\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\$\[\033[0m\] "
     '';
   };

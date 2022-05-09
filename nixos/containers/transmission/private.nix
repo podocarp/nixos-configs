c879f4e57b5c741789df2f45de711699bece8981
jxd <xdjiaxd@gmail.com>
2022-05-09 22:34:33 +0800

    moving stuff around

--- nixos/containers/transmission/private.nix
+++ nixos/containers/transmission/private.nix
@@ -9,37 +9,41 @@ in
     volumes = [
       "/tank/local/transmission_priv:${dir}"
       "/tank/private/Downloads:/Downloads"
+      "/tank/private/Downloaded:/Downloaded"
       "${config.sops.secrets.openvpn-credentials.path}:/config/openvpn-credentials.txt"
     ];
     ports = [ "${toString port}:9091" ];
     extraOptions = [ "--cap-add=NET_ADMIN" ];
     environment =
     {
-      PUID = builtins.toString config.users.users."pengu".uid;
-      PGID = builtins.toString config.users.groups."users".gid;
+      GLOBAL_APPLY_PERMISSIONS = "false";
+      LOCAL_NETWORK= "192.168.1.0/24";
+      NORDVPN_CATEGORY = "standard";
+      NORDVPN_COUNTRY = "CH";
+      NORDVPN_PROTOCOL = "UDP";
+      OPENVPN_PASSWORD = "**None**";
       OPENVPN_PROVIDER = "NORDVPN";
       OPENVPN_USERNAME = "**None**";
-      OPENVPN_PASSWORD = "**None**";
-      NORDVPN_COUNTRY = "CH";
-      NORDVPN_CATEGORY = "standard";
-      NORDVPN_PROTOCOL = "UDP";
-      LOCAL_NETWORK= "192.168.1.0/24";
+      PGID = builtins.toString config.users.groups."users".gid;
+      PUID = builtins.toString config.users.users."pengu".uid;
 
-      TRANSMISSION_HOME = dir;
-      TRANSMISSION_DOWNLOAD_DIR = "/Downloads";
-      TRANSMISSION_INCOMPLETE_DIR_ENABLED = "false";
-      TRANSMISSION_RPC_HOST_WHITELIST = "*.home.com";
-      TRANSMISSION_RPC_WHITELIST = "192.168.1.*,127.0.0.1";
-      TRANSMISSION_RPC_AUTHENTICATION_REQUIRED = "true";
-      TRANSMISSION_RPC_USERNAME = "pengu";
-      TRANSMISSION_RPC_PASSWORD = "pengu";
-      TRANSMISSION_PEER_PORT_RANDOM_ON_START = "true";
+      TRANSMISSION_DOWNLOAD_DIR = "/Downloaded";
       TRANSMISSION_DOWNLOAD_QUEUE_ENABLED = "true";
       TRANSMISSION_DOWNLOAD_QUEUE_SIZE = "20";
+      TRANSMISSION_HOME = dir;
+      TRANSMISSION_INCOMPLETE_DIR = "/Downloads";
+      TRANSMISSION_INCOMPLETE_DIR_ENABLED = "true";
+      TRANSMISSION_PEER_PORT_RANDOM_ON_START = "true";
       TRANSMISSION_QUEUE_STALLED_ENABLED = "true";
       TRANSMISSION_QUEUE_STALLED_MINUTES = "10";
-      TRANSMISSION_RATIO_LIMIT_ENABLED = "true";
       TRANSMISSION_RATIO_LIMIT = "1";
+      TRANSMISSION_RATIO_LIMIT_ENABLED = "true";
+      TRANSMISSION_RPC_AUTHENTICATION_REQUIRED = "true";
+      TRANSMISSION_RPC_HOST_WHITELIST = "*.home.com";
+      TRANSMISSION_RPC_PASSWORD = "pengu";
+      TRANSMISSION_RPC_USERNAME = "pengu";
+      TRANSMISSION_RPC_WHITELIST = "192.168.1.*,127.0.0.1";
+      TRANSMISSION_WEB_UI = "flood-for-transmission";
     };
   };
 
